2896b07a96edc33d91957cbc5931398cca4f1c11
midohima2006 <midohima2006@gmail.com>
2025-03-31 00:00:30 +0200

    Files reformatted!

--- script.js
+++ script.js
@@ -2,19 +2,19 @@ const chckBtn = document.getElementById("check-btn");
 const resultsDiv = document.getElementById("results-div");
 const result = document.getElementById("result");
 const chckpd = () => {
-const inputField = document.getElementById("text-input");
-const rawTxt = inputField.value;
-if (rawTxt === "") {
-  alert("Please input a value");
-} else {
-const cleanTxt = rawTxt.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
-const reverseTxt = cleanTxt.split('').reverse().join('');
-resultsDiv.style.display = "flex";
-if (reverseTxt === cleanTxt) {
-  result.innerText = `${rawTxt} is a palindrome!`
-} else {
-  result.innerText = `${rawTxt} is not a palindrome!`
-}
-}
-}
+  const inputField = document.getElementById("text-input");
+  const rawTxt = inputField.value;
+  if (rawTxt === "") {
+    alert("Please input a value");
+  } else {
+    const cleanTxt = rawTxt.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+    const reverseTxt = cleanTxt.split("").reverse().join("");
+    resultsDiv.style.display = "flex";
+    if (reverseTxt === cleanTxt) {
+      result.innerText = `${rawTxt} is a palindrome!`;
+    } else {
+      result.innerText = `${rawTxt} is not a palindrome!`;
+    }
+  }
+};
 chckBtn.addEventListener("click", chckpd);
